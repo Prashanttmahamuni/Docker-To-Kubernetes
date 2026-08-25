3e38a3da29234d15c4b81aa41782410dc1499010
Prashant Mahamuni <164340985+Prashanttmahamuni@users.noreply.github.com>
2026-08-25 19:07:33 +0530

    Fix safelist formatting in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import animate from 'tailwindcss-animate';
 
 export default {
   darkMode: 'class',
-  safelist: ['dark'],
+  safelist: ['dark'],    
   prefix: '',
   content: [
     './content/**/*',
