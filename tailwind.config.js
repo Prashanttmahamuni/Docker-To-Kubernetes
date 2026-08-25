42b69eb09035b5f0d32a41acca870e13302235a0
Prashant Mahamuni <164340985+Prashanttmahamuni@users.noreply.github.com>
2026-08-25 19:07:40 +0530

    Fix formatting in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
       },
       screens: {
         sm: '640px',
-        md: '768px',
+        md: '768px',      
         lg: '1024px',
         xl: '1280px',
         '2xl': '1400px',
